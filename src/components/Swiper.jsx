e86e325b67279a52ea1a49894f677a6197331d45
derya <lacin>
2025-01-03 19:26:20 +0100

    dd

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
+import Login from '../assets/Login.png';
 import starWars from '../assets/starWars.png';
 import Photo1 from '../assets/photo1.jpg';
 import Stok from '../assets/projeStok.png';
@@ -15,7 +15,9 @@ import food from '../assets/food.png';
 import verticalCaroussel from '../assets/verticalCaroussel.png';
 import todo from '../assets/todo.png';
 import parallax from '../assets/parallax.png';
-
+import game from '../assets/game.png';
+import game1 from '../assets/game1.png';
+import shoes from '../assets/shoes.png';
 function SwiperComponent() {
   const slides = [
     { src: parallax, link: 'https://ocean2024d.github.io/strechText/', alt: 'parallax Project' },
@@ -23,12 +25,17 @@ function SwiperComponent() {
     { src: bistro, link: 'https://ocean2024d.github.io/Bistro-ThemeForest-Exercise/', alt: 'Bistro Theme' },
     { src: food, link: 'https://ocean2024d.github.io/food/', alt: 'Food Project' },
     { src: Stok, link: 'https://ocean2024d.github.io/stok/', alt: 'Stok Project' },
-    { src: todo, link: 'https://ocean2024d.github.io/react-form/', alt: 'To-Do App' },
+    { src: Form, link: 'https://ocean2024d.github.io/react-form/', alt: 'Form' },
     { src: starWars, link: 'https://ocean2024d.github.io/starWars/', alt: 'Star Wars Project' },
     { src: verticalCaroussel, link: 'https://ocean2024d.github.io/verticalCarrouselGsap/', alt: 'verticalCarousselGsap Project' },
-    
+    { src: Login, link: ' https://ocean2024d.github.io/LoginPage-React/' , alt: 'Login Page' },
+    { src: game, link: 'https://ocean2024d.github.io/game1/', alt: 'game Project' },
+    { src: game1, link: 'https://ocean2024d.github.io/game/', alt: 'game1 Project' },
+    { src: shoes, link: 'https://ocean2024d.github.io/Shoes/' , alt: 'shoes Page' }
+  
+  
+  
   ]
-  ;
 
   return (
     <div className="swiper-container">
